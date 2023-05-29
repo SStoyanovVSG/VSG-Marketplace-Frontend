6ca613fccfe9395a7ffdf266f540321ded2beff6
Stiliyan Stoyanov <SStoyanov@vsgbg.com>
2023-05-29 09:50:57 +0300

    Configs

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -3,8 +3,8 @@ import { PublicClientApplication } from "@azure/msal-browser/dist/index.js";
 export const msalConfig = {
   auth: {
     clientId: "86ceffd4-8632-4677-bbb6-e7badafa26ec",
-    authority:
-      "https://login.microsoftonline.com/50ae1bf7-d359-4aff-91ac-b084dc52111e",
+    authority: "https://login.microsoftonline.com/50ae1bf7-d359-4aff-91ac-b084dc52111e",
+      redirectUri: 'https://vsg-marketplace.vercel.app/',
   },
   cache: {
     cacheLocation: "sessionStorage",
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  build: {
+    outDir: "dist",
+
+    emptyOutDir: true,
+
+    rollupOptions: {
+      output: {
+        chunkFileNames: "chunks/[name].js",
+
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+
   plugins: [react()],
-})
+});
