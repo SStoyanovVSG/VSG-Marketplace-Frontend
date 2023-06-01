f8923488fd80c5c2165c79ad0faf92e997c119f0
Stiliyan Stoyanov <SStoyanov@vsgbg.com>
2023-06-01 09:28:22 +0300

    FIxed deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     outDir: "dist",
-
     emptyOutDir: true,
-
     rollupOptions: {
       output: {
         chunkFileNames: "chunks/[name].js",
-
         entryFileNames: "[name].js",
       },
     },
   },
-
   plugins: [react()],
 });
